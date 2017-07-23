f551e787620fe5ae7967d793b4d433fcbc521eae
Nikunj Agarwal <nikunjagarwal792@gmail.com>
2017-07-23 11:27:07 +0530

    Heroku

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ var express = require('express');
 //create Our App.
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+app.use(function(req,resp,next){
+	if(req.header['x-forwarded-proto']=='http'){
+		next();
+	}else{
+		res.redirect('http://'+req.hostname + req.url);
+	}
+});
 app.use(express.static('public'));
-app.listen(3000,function(){
-	console.log("Server is UP");
+app.listen(PORT,function(){
+	console.log("Server is UP"+PORT);
 })
